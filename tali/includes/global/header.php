4faf28950f42e1b899d815e6fa8324dbc345c4dd
KC Grimes <kc.grimes@gmail.com>
2017-12-17 17:10:05 +0000

    -Put banner inside container and formatted it
    git-svn-id: file:///E/Users/KC%20Grimes/Large%20Documents/SVN%20Repositories/svn_TALI/trunk@58 7e833b05-e863-334a-8b38-2db3a1af8c50

--- tali/includes/global/header.php
+++ tali/includes/global/header.php
@@ -1,15 +1,15 @@
 <?php
 echo "
 	<header>
-		<a href=\"".$_SESSION["TALI_Domain_URL"]."".$_SESSION["TALI_ROOT_URL"]."/index.php\">
-			<img src=\"".$_SESSION["TALI_Domain_URL"]."".$_SESSION["TALI_ROOT_URL"]."/images/display/TALIBanner.png\" alt=\"TALI Banner\" name=\"Team Administration/Logistics Interface\" style=\"display:block;max-width:100%;padding-top:10px;padding-bottom:10px;margin-left:auto;margin-right:auto;\"/>
-		</a>
+		<div class=\"tali-container\">
+			<a href=\"".$_SESSION["TALI_Domain_URL"]."".$_SESSION["TALI_ROOT_URL"]."/index.php\">
+				<img class=\"tali-header-banner_img\" src=\"".$_SESSION["TALI_Domain_URL"]."".$_SESSION["TALI_ROOT_URL"]."/images/display/TALIBanner.png\" alt=\"TALI Banner\" name=\"Team Administration/Logistics Interface\"/>
+			</a>
 ";
 
 //If logged in, display some more information
 if (isset($_SESSION['username'])) {
 	echo "
-		<div class=\"tali-container\">
 			<table class=\"tali-header-text_table\">
 				<col width=\"50%\">
 				<col width=\"50%\">
@@ -18,11 +18,11 @@ if (isset($_SESSION['username'])) {
 					<td style=\"text-align:right;\">Logged in as ".$_SESSION['username']." (<a href=\"".$_SESSION['TALI_Domain_URL']."".$_SESSION['TALI_ROOT_URL']."/account.php\">Account</a>) (<a href=\"".$_SESSION['TALI_Domain_URL']."".$_SESSION['TALI_ROOT_URL']."/login.php\">Log Out</a>)</td>
 				</tr>
 			</table>
-		</div>
 	";
 }
 
 echo "
+		</div>
 	</header>
 ";
 ?>
