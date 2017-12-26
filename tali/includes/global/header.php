1b71cbcda2d98b48bbf0bc9d1d184a470d1c0bc1
KC Grimes <kc.grimes@gmail.com>
2017-12-26 17:36:00 +0000

    -Replaced header text table with divs
    git-svn-id: file:///E/Users/KC%20Grimes/Large%20Documents/SVN%20Repositories/svn_TALI/trunk@77 7e833b05-e863-334a-8b38-2db3a1af8c50

--- tali/includes/global/header.php
+++ tali/includes/global/header.php
@@ -10,14 +10,10 @@ echo "
 //If logged in, display some more information
 if (isset($_SESSION['username'])) {
 	echo "
-			<table class=\"tali-header-text_table\">
-				<col width=\"50%\">
-				<col width=\"50%\">
-				<tr>
-					<td style=\"text-align:left;\"><a href=\"javascript:history.back()\">Go Back</a></td>
-					<td style=\"text-align:right;\">Logged in as ".$_SESSION['username']." (<a href=\"".$_SESSION['TALI_Domain_URL']."".$_SESSION['TALI_ROOT_URL']."/account.php\">Account</a>) (<a href=\"".$_SESSION['TALI_Domain_URL']."".$_SESSION['TALI_ROOT_URL']."/login.php\">Log Out</a>)</td>
-				</tr>
-			</table>
+			<div>
+				<p class=\"tali-header-goback\"><a href=\"javascript:history.back()\">Go Back</a></p>
+				<p class=\"tali-header-account\">Logged in as ".$_SESSION['username']." (<a href=\"".$_SESSION['TALI_Domain_URL']."".$_SESSION['TALI_ROOT_URL']."/account.php\">Account</a>) (<a href=\"".$_SESSION['TALI_Domain_URL']."".$_SESSION['TALI_ROOT_URL']."/login.php\">Log Out</a>)</p>
+			</div>
 	";
 }
 
