65325c34d95f3f7faebe33b45d431c34b65d9eb0
KC Grimes <kc.grimes@gmail.com>
2018-01-05 15:39:46 +0000

    Replaced missed h4 closing tags with correct h1 closing tag Added Kent "KC" Grimes to copyright notice Added @gmail.com for e-mail contact
    git-svn-id: file:///E/Users/KC%20Grimes/Large%20Documents/SVN%20Repositories/svn_TALI/trunk@91 7e833b05-e863-334a-8b38-2db3a1af8c50

--- tali/includes/global/footer.php
+++ tali/includes/global/footer.php
@@ -4,7 +4,7 @@ echo "
 	<footer>
 		<div class=\"tali-container\">
 			| <a href=\"".TALI_DOMAIN_URL."".TALI_URI."/termsconditions.php\" style=\"color: #000000\">Terms and Conditions</a> | <a href=\"".TALI_DOMAIN_URL."".TALI_URI."/privacy.php\" style=\"color: #000000\">Privacy Statement</a> |<br>
-			Copyright © 2014-$curYear Travis County Search &amp; Rescue. All Rights Reserved.
+			Copyright © 2014-$curYear Kent \"KC\" Grimes. All Rights Reserved.
 		</div>
 	</footer>
 ";
--- tali/includes/privacy/privacy-main.php
+++ tali/includes/privacy/privacy-main.php
@@ -4,48 +4,48 @@ echo "
 		<div class=\"tali-container\">
 			<div class=\"tali-page-frame\">
 				<h1>Privacy Statement</h1>
-				<p>Travis County Search & Rescue is committed to protecting your privacy and developing technology that gives you the most powerful and safe online experience. This Statement of Privacy applies to the Travis County Search & Rescue Web site and governs data collection and usage. By using the Travis County Search & Rescue website, you consent to the data practices described in this statement.</p>
-				<h1>Collection of your Personal Information</h4>
+				<p>The Team Administrative/Logistics Interface, henceforth TALI, is committed to protecting your privacy and developing technology that gives you the most powerful and safe online experience. This Statement of Privacy applies to the TALI Web site and governs data collection and usage. By using the TALI website, you consent to the data practices described in this statement.</p>
+				<h1>Collection of your Personal Information</h1>
 
-				<p>Travis County Search & Rescue collects personally identifiable information, such as your e-mail address, name, home or work address or telephone number. Travis County Search & Rescue also collects anonymous demographic information, which is not unique to you, such as your ZIP code, age, gender, preferences, interests and favorites.</p>
+				<p>TALI collects personally identifiable information, such as your e-mail address, name, home or work address or telephone number. TALI also collects anonymous demographic information, which is not unique to you, such as your ZIP code, age, gender, preferences, interests and favorites.</p>
 
-				<p>There is also information about your computer hardware and software that is automatically collected by Travis County Search & Rescue. This information can include: your IP address, browser type, domain names, access times and referring Web site addresses. This information is used by Travis County Search & Rescue for the operation of the service, to maintain quality of the service, and to provide general statistics regarding use of the Travis County Search & Rescue Web site.</p>
+				<p>There is also information about your computer hardware and software that is automatically collected by TALI. This information can include: your IP address, browser type, domain names, access times and referring Web site addresses. This information is used by TALI for the operation of the service, to maintain quality of the service, and to provide general statistics regarding use of the TALI Web site.</p>
 
-				<p>Please keep in mind that if you directly disclose personally identifiable information or personally sensitive data through Travis County Search & Rescue public message boards, this information may be collected and used by others. Note: Travis County Search & Rescue does not read any of your private online communications.</p>
+				<p>Please keep in mind that if you directly disclose personally identifiable information or personally sensitive data through TALI public message boards, this information may be collected and used by others. Note: TALI does not read any of your private online communications.</p>
 
-				<p>Travis County Search & Rescue encourages you to review the privacy statements of Web sites you choose to link to from Travis County Search & Rescue so that you can understand how those Web sites collect, use and share your information. Travis County Search & Rescue is not responsible for the privacy statements or other content on Web sites outside of the Travis County Search & Rescue and Travis County Search & Rescue family of Web sites.</p>
+				<p>TALI encourages you to review the privacy statements of Web sites you choose to link to from TALI so that you can understand how those Web sites collect, use and share your information. TALI is not responsible for the privacy statements or other content on Web sites outside of the TALI and TALI family of Web sites.</p>
 
-				<h1>Use of your Personal Information</h4>
+				<h1>Use of your Personal Information</h1>
 
-				<p>Travis County Search & Rescue collects and uses your personal information to operate the Travis County Search & Rescue Web site and deliver the services you have requested. Travis County Search & Rescue also uses your personally identifiable information to inform you of other products or services available from Travis County Search & Rescue and its affiliates. Travis County Search & Rescue may also contact you via surveys to conduct research about your opinion of current services or of potential new services that may be offered.</p>
+				<p>TALI collects and uses your personal information to operate the TALI Web site and deliver the services you have requested. TALI also uses your personally identifiable information to inform you of other products or services available from TALI and its affiliates. TALI may also contact you via surveys to conduct research about your opinion of current services or of potential new services that may be offered.</p>
 
-				<p>Travis County Search & Rescue does not sell, rent or lease its customer lists to third parties. Travis County Search & Rescue may, from time to time, contact you on behalf of external business partners about a particular offering that may be of interest to you. In those cases, your unique personally identifiable information (e-mail, name, address, telephone number) is not transferred to the third party. In addition, Travis County Search & Rescue may share data with trusted partners to help us perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services to Travis County Search & Rescue, and they are required to maintain the confidentiality of your information.</p>
+				<p>TALI does not sell, rent or lease its customer lists to third parties. TALI may, from time to time, contact you on behalf of external business partners about a particular offering that may be of interest to you. In those cases, your unique personally identifiable information (e-mail, name, address, telephone number) is not transferred to the third party. In addition, TALI may share data with trusted partners to help us perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services to TALI, and they are required to maintain the confidentiality of your information.</p>
 
-				<p>Travis County Search & Rescue does not use or disclose sensitive personal information, such as race, religion, or political affiliations, without your explicit consent.</p>
+				<p>TALI does not use or disclose sensitive personal information, such as race, religion, or political affiliations, without your explicit consent.</p>
 
-				<p>Travis County Search & Rescue keeps track of the Web sites and pages our customers visit within Travis County Search & Rescue, in order to determine what Travis County Search & Rescue services are the most popular. This data is used to deliver customized content and advertising within Travis County Search & Rescue to customers whose behavior indicates that they are interested in a particular subject area.</p>
+				<p>TALI keeps track of the Web sites and pages our customers visit within TALI, in order to determine what TALI services are the most popular. This data is used to deliver customized content and advertising within TALI to customers whose behavior indicates that they are interested in a particular subject area.</p>
 
-				<p>Travis County Search & Rescue Web sites will disclose your personal information, without notice, only if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on Travis County Search & Rescue or the site; (b) protect and defend the rights or property of Travis County Search & Rescue; and, (c) act under exigent circumstances to protect the personal safety of users of Travis County Search & Rescue, or the public.</p>
+				<p>TALI Web sites will disclose your personal information, without notice, only if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on TALI or the site; (b) protect and defend the rights or property of TALI; and, (c) act under exigent circumstances to protect the personal safety of users of TALI, or the public.</p>
 
-				<h1>Use of Cookies</h4>
+				<h1>Use of Cookies</h1>
 
-				<p>The Travis County Search & Rescue Web site use \"cookies\" to help you personalize your online experience. A cookie is a text file that is placed on your hard disk by a Web page server. Cookies cannot be used to run programs or deliver viruses to your computer. Cookies are uniquely assigned to you, and can only be read by a web server in the domain that issued the cookie to you.</p>
+				<p>The TALI Web sites use \"cookies\" to help you personalize your online experience. A cookie is a text file that is placed on your hard disk by a Web page server. Cookies cannot be used to run programs or deliver viruses to your computer. Cookies are uniquely assigned to you, and can only be read by a web server in the domain that issued the cookie to you.</p>
 
-				<p>One of the primary purposes of cookies is to provide a convenience feature to save you time. The purpose of a cookie is to tell the Web server that you have returned to a specific page. For example, if you personalize Travis County Search & Rescue pages, or register with Travis County Search & Rescue site or services, a cookie helps Travis County Search & Rescue to recall your specific information on subsequent visits. This simplifies the process of recording your personal information, such as billing addresses, shipping addresses, and so on. When you return to the same Travis County Search & Rescue Web site, the information you previously provided can be retrieved, so you can easily use the Travis County Search & Rescue features that you customized.</p>
+				<p>One of the primary purposes of cookies is to provide a convenience feature to save you time. The purpose of a cookie is to tell the Web server that you have returned to a specific page. For example, if you personalize TALI pages, or register with TALI site or services, a cookie helps TALI to recall your specific information on subsequent visits. This simplifies the process of recording your personal information, such as billing addresses, shipping addresses, and so on. When you return to the same TALI Web site, the information you previously provided can be retrieved, so you can easily use the TALI features that you customized.</p>
 
-				<p>You have the ability to accept or decline cookies. Most Web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. If you choose to decline cookies, you may not be able to fully experience the interactive features of the Travis County Search & Rescue services or Web sites you visit.</p>
+				<p>You have the ability to accept or decline cookies. Most Web browsers automatically accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. If you choose to decline cookies, you may not be able to fully experience the interactive features of the TALI services or Web sites you visit.</p>
 
-				<h1>Security of your Personal Information</h4>
+				<h1>Security of your Personal Information</h1>
 
-				<p>Travis County Search & Rescue secures your personal information from unauthorized access, use or disclosure. Travis County Search & Rescue secures the personally identifiable information you provide on computer servers in a controlled, secure environment, protected from unauthorized access, use or disclosure. When personal information (such as a credit card number) is transmitted to other Web sites, it is protected through the use of encryption, such as the Secure Socket Layer (SSL) protocol.</p>
+				<p>TALI secures your personal information from unauthorized access, use or disclosure. TALI secures the personally identifiable information you provide on computer servers in a controlled, secure environment, protected from unauthorized access, use or disclosure. When personal information (such as a credit card number) is transmitted to other Web sites, it is protected through the use of encryption, such as the Secure Socket Layer (SSL) protocol.</p>
 
-				<h1>Changes to this Statement</h4>
+				<h1>Changes to this Statement</h1>
 
-				<p>Travis County Search & Rescue will occasionally update this Statement of Privacy to reflect company and customer feedback. Travis County Search & Rescue encourages you to periodically review this Statement to be informed of how Travis County Search & Rescue is protecting your information.</p>
+				<p>TALI will occasionally update this Statement of Privacy to reflect company and customer feedback. TALI encourages you to periodically review this Statement to be informed of how TALI is protecting your information.</p>
 
-				<h1>Contact Information</h4>
+				<h1>Contact Information</h1>
 
-				<p>Travis County Search & Rescue welcomes your comments regarding this Statement of Privacy. If you believe that Travis County Search & Rescue has not adhered to this Statement, please contact Travis County Search & Rescue at <a href=\"mailto:kc.grimes@tcsar.org\">kc.grimes@tcsar.org</a>. We will use commercially reasonable efforts to promptly determine and remedy the problem.</p> 
+				<p>TALI welcomes your comments regarding this Statement of Privacy. If you believe that TALI has not adhered to this Statement, please contact TALI at <a href=\"mailto:kc.grimes@gmail.com\">kc.grimes@gmail.com</a>. We will use commercially reasonable efforts to promptly determine and remedy the problem.</p> 
 			</div>
 		</div>
 	</main>
