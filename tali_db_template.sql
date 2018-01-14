f39842d969f7e210f32f66b0c4e6c87ef5f585e8
KC Grimes <kc.grimes@gmail.com>
2018-01-14 12:33:38 -0600

    Name database in template "tali_db" (remove 3rd ID name)

--- tali_db_template.sql
+++ tali_db_template.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `thirdid_website_dev`
+-- Database: `tali_db`
 --
 
 -- --------------------------------------------------------
